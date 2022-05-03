d77b2781d03dce8565e4cacb886fd74dc2dda733
Anton21002 <97591679+Anton21002@users.noreply.github.com>
2022-05-03 12:02:47 +0300

    [WIP] independent search engine

--- ElectionMachine/src/main/java/app/QuestionSearch.java
+++ ElectionMachine/src/main/java/app/QuestionSearch.java
@@ -1,21 +1,42 @@
 package app;
 
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 import javax.servlet.RequestDispatcher;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import app.dao.FitKysymys;
+
+
+
+
+
 public class QuestionSearch {
+	EntityManagerFactory emf=Persistence.createEntityManagerFactory("ElectionMachine");
 	@GET
 	@Path("")
-	public void Search ()
+	public void Search (PrintWriter out,String searchImport)
 	{	
 	EntityManager em=emf.createEntityManager();
     em.getTransaction().begin();
-    Kid kid=em.find(Kid.class, 2);
+    Query q=em.createQuery("SELECT * from Kysymukset where Kysymus LIKE :paramName");
+	q.setParameter("paramName", searchImport);
+    List<FitKysymys> list=q.getResultList();
     em.getTransaction().commit();
-    request.setAttribute("kid", kid);
-    RequestDispatcher rd=request.getRequestDispatcher("./jsp/readonekidnotfish.jsp");
-	rd.forward(request, response);
-}}
+    em.close();
+    RequestDispatcher rd=q.getRequestDispatcher("./jsp/Searchtab.jsp");
+	PrintFitting(out, list);
+}
 //SELECT * from Kysymukset where Kysymus LIKE %searchinp(var)%
+public void PrintFitting (PrintWriter out,List<Kysymys> list) {
+	
+}
+
+}
---
+++ ElectionMachine/src/main/java/app/dao/FitKysymys.java
@@ -0,0 +1,9 @@
+package app.dao;
+
+import java.io.Serializable;
+
+public class FitKysymys implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String 
+
+}
